f37ad6068dbda00cbe2d5ce8bc21f0d2a3f26df5
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2021-04-24 17:26:43 +0000

    add container sizes

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log("we're on")
+
+const containers = {
+    "8ft": {
+        length: 2.43,
+        width: 2.20
+    },
+    "10ft": {
+        length: 2.99,
+        width: 2.44
+    },
+    "20ft": {
+        length: 6.06,
+        width: 2.44
+    },
+    "40ft": {
+        length: 12.2,
+        width: 2.44
+    }
+}
