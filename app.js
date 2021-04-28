6932f40bfd31b7250b36a1a06bb34d09e03d0b6a
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2021-04-28 21:04:14 +0000

    show pallets dimentions

--- app.js
+++ app.js
@@ -79,12 +79,49 @@ const addNewPalletFct = e => {
 const getPallet = (pName, pLength, pWidth) => {
     const newPallet = document.createElement('div')
     const p = document.createElement('p')
+    const sideLineContainer = document.createElement('div')
+    const bottomLineContainer = document.createElement('span')
+    const sideLineText = document.createElement('span')
+    const bottomLineText = document.createElement('div')
+    const sideLine = document.createElement('div')
+    const bottomLine = document.createElement('div')
+    const sideLineArrowStart = document.createElement('div')
+    const sideLineArrowEnd = document.createElement('div')
+    const bottomLineArrowStart = document.createElement('div')
+    const bottomLineArrowEnd = document.createElement('div')
+
+    // p
     p.innerText = pName;
     newPallet.appendChild(p)
-    newPallet.classList.add('pallet')
 
+    // side line
+    sideLineContainer.classList.add('dimentions', 'side-line-container')
+    sideLineText.innerText = pWidth + 'm';
+    sideLineContainer.appendChild(sideLineText)
+    sideLine.classList.add('line', 'side-line')
+    sideLineArrowStart.classList.add('arrow', 'arrow-start')
+    sideLineArrowEnd.classList.add('arrow', 'arrow-end')
+    sideLine.appendChild(sideLineArrowStart)
+    sideLine.appendChild(sideLineArrowEnd)
+    sideLineContainer.appendChild(sideLine)
+
+    // bottom line
+    bottomLineContainer.classList.add('dimentions', 'bottom-line-container')
+    bottomLineText.innerText = pLength + 'm';
+    bottomLineContainer.appendChild(bottomLineText)
+    bottomLine.classList.add('line', 'bottom-line')
+    bottomLineArrowStart.classList.add('arrow', 'arrow-start')
+    bottomLineArrowEnd.classList.add('arrow', 'arrow-end')
+    bottomLine.appendChild(bottomLineArrowStart)
+    bottomLine.appendChild(bottomLineArrowEnd)
+    bottomLineContainer.appendChild(bottomLine)
+
+    // pallet
+    newPallet.classList.add('pallet')
     newPallet.style.width = pLength * 100 + 'px'
     newPallet.style.height = pWidth * 100 + 'px'
+    newPallet.appendChild(sideLineContainer)
+    newPallet.appendChild(bottomLineContainer)
 
     return newPallet
 }
