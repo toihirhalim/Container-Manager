a94d09d17013f10e4e408c0322ea2779eb671d3d
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2021-04-28 21:10:42 +0000

    move getPallet func to library.js and renamme it to generatePallet

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const getPalletsAndDisplay = () => {
         pallets = p
         Object.keys(pallets).map(key => {
             let pallet = pallets[key]
-            const newPallet = getPallet(key, pallet.length, pallet.width)
+            const newPallet = generatePallet(key, pallet.length, pallet.width)
 
             pailletContainer.appendChild(newPallet)
         });
@@ -69,61 +69,11 @@ const addNewPalletFct = e => {
     setLocalObject('palletsSequence', palletsSequence)
 
     //add new pallet
-    const newPallet = getPallet(name, length, width)
+    const newPallet = generatePallet(name, length, width)
 
     pailletContainer.appendChild(newPallet)
 
     closeNewPalletPannel()
-}
-
-const getPallet = (pName, pLength, pWidth) => {
-    const newPallet = document.createElement('div')
-    const p = document.createElement('p')
-    const sideLineContainer = document.createElement('div')
-    const bottomLineContainer = document.createElement('span')
-    const sideLineText = document.createElement('span')
-    const bottomLineText = document.createElement('div')
-    const sideLine = document.createElement('div')
-    const bottomLine = document.createElement('div')
-    const sideLineArrowStart = document.createElement('div')
-    const sideLineArrowEnd = document.createElement('div')
-    const bottomLineArrowStart = document.createElement('div')
-    const bottomLineArrowEnd = document.createElement('div')
-
-    // p
-    p.innerText = pName;
-    newPallet.appendChild(p)
-
-    // side line
-    sideLineContainer.classList.add('dimentions', 'side-line-container')
-    sideLineText.innerText = pWidth + 'm';
-    sideLineContainer.appendChild(sideLineText)
-    sideLine.classList.add('line', 'side-line')
-    sideLineArrowStart.classList.add('arrow', 'arrow-start')
-    sideLineArrowEnd.classList.add('arrow', 'arrow-end')
-    sideLine.appendChild(sideLineArrowStart)
-    sideLine.appendChild(sideLineArrowEnd)
-    sideLineContainer.appendChild(sideLine)
-
-    // bottom line
-    bottomLineContainer.classList.add('dimentions', 'bottom-line-container')
-    bottomLineText.innerText = pLength + 'm';
-    bottomLineContainer.appendChild(bottomLineText)
-    bottomLine.classList.add('line', 'bottom-line')
-    bottomLineArrowStart.classList.add('arrow', 'arrow-start')
-    bottomLineArrowEnd.classList.add('arrow', 'arrow-end')
-    bottomLine.appendChild(bottomLineArrowStart)
-    bottomLine.appendChild(bottomLineArrowEnd)
-    bottomLineContainer.appendChild(bottomLine)
-
-    // pallet
-    newPallet.classList.add('pallet')
-    newPallet.style.width = pLength * 100 + 'px'
-    newPallet.style.height = pWidth * 100 + 'px'
-    newPallet.appendChild(sideLineContainer)
-    newPallet.appendChild(bottomLineContainer)
-
-    return newPallet
 }
 
 const checkPalletName = e => {
